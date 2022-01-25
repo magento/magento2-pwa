b05ad65c5fa110b480a6c119b80aad52a1049e8c
Mikhaël Bois <mbois@absolunet.com>
2022-01-25 11:09:03 -0500

    [PWA-2063] Add reCaptcha to the Forgot Password Forms (#20)
    * PWA-2063 Plugin to add missing config
    
    * PWA-2063 Add unit tests
    
    * PWA-2063 Update namespace
    
    * PWA-2063 Clean code
    
    * PWA-2063 Fix deps
    
    * PWA-2063 Fix deps + Add phpmd ignore
    
    Co-authored-by: Eric Oeur <31492142+ericeoeur@users.noreply.github.com>
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>

---
+++ ReCaptchaPwa/Plugin/Model/WebapiConfigProvider.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\ReCaptchaPwa\Plugin\Model;
+
+use Magento\ReCaptchaUi\Model\IsCaptchaEnabledInterface;
+use Magento\ReCaptchaUi\Model\ValidationConfigResolverInterface;
+use Magento\ReCaptchaValidationApi\Api\Data\ValidationConfigInterface;
+use Magento\ReCaptchaWebapiApi\Api\Data\EndpointInterface;
+
+class WebapiConfigProvider
+{
+    private const RESET_PASSWORD_CAPTCHA_ID = 'customer_forgot_password';
+
+    /**
+     * @var IsCaptchaEnabledInterface
+     */
+    private $isEnabled;
+
+    /**
+     * @var ValidationConfigResolverInterface
+     */
+    private $configResolver;
+
+    /**
+     * @param IsCaptchaEnabledInterface $isEnabled
+     * @param ValidationConfigResolverInterface $configResolver
+     */
+    public function __construct(IsCaptchaEnabledInterface $isEnabled, ValidationConfigResolverInterface $configResolver)
+    {
+        $this->isEnabled = $isEnabled;
+        $this->configResolver = $configResolver;
+    }
+
+    /**
+     * Adds missing config
+     *
+     * @param \Magento\ReCaptchaCustomer\Model\WebapiConfigProvider $subject
+     * @param ValidationConfigInterface|null $result
+     * @param EndpointInterface $endpoint
+     * @return ValidationConfigInterface|null
+     * @throws \Magento\Framework\Exception\InputException
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterGetConfigFor(
+        \Magento\ReCaptchaCustomer\Model\WebapiConfigProvider $subject,
+        $result,
+        EndpointInterface $endpoint
+    ): ?ValidationConfigInterface {
+        $serviceClass = $endpoint->getServiceClass();
+        $serviceMethod = $endpoint->getServiceMethod();
+
+        //phpcs:disable Magento2.PHP.LiteralNamespaces
+        if ($serviceMethod === 'resetPassword'
+            || $serviceMethod === 'initiatePasswordReset'
+            || $serviceClass === 'Magento\CustomerGraphQl\Model\Resolver\ResetPassword'
+            || $serviceClass === 'Magento\CustomerGraphQl\Model\Resolver\RequestPasswordResetEmail') {
+            if ($this->isEnabled->isCaptchaEnabledFor(self::RESET_PASSWORD_CAPTCHA_ID)) {
+                return $this->configResolver->get(self::RESET_PASSWORD_CAPTCHA_ID);
+            }
+        }
+        //phpcs:enable Magento2.PHP.LiteralNamespaces
+
+        return $result;
+    }
+}
--- ReCaptchaPwa/README.md
+++ ReCaptchaPwa/README.md
@@ -1,3 +1,3 @@
 # Magento_ReCaptchaPwa
 
-**Magento_ReCaptchaPwa** adds warnings to Magento ReCaptcha modules.
+**Magento_ReCaptchaPwa** adds warnings to Magento ReCaptcha modules and adds config validation for the Request Password Reset Form.
